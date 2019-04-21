1a9f05277be5753a73db5dd8a0e82c62f438c298
Burlak Ilia <burlakilia@bk.ru>
2019-04-21 16:09:30 +0300

    Доарботал получение oauth токен

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { AuthRoute } = require('./auth');
 const { WebhooksRoute } = require('./webhooks');
 const { PddToken } = require('./libs/pdd');
 
+const serve = require('koa-static');
 const app = new Koa();
 const router = new Router();
 
@@ -11,8 +12,10 @@ const pdd = new PddToken(process.env.PDD_TOKEN);
 const auth = new AuthRoute(router, 'auth', pdd);
 const webhooks = new WebhooksRoute(router, 'webhooks', pdd);
 
+
 app
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(serve('./src'));
 
 app.listen(process.env.PORT || 3005);
